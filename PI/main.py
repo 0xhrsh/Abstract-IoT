3d984d672c0a8965f1ec204a5f436d1714870e36
harsh-not-haarsh <anand.2@iitj.ac.in>
2021-02-15 15:23:37 +0530

    fixed some errors

--- PI/main.py
+++ PI/main.py
@@ -3,12 +3,11 @@ import requests
 import time
 import json
 import socket
-import os
 
 HEADER = 'RAP\r\n\r\n\r\n\r\n\r\n\r\nPI_ID: 42069\r\nconfig_version: {}\r\n\r\n'
 
 
-HOST = "127.0.0.1"
+HOST = "192.168.1.6"
 PORT = 8888
 HUB_DOMAIN = "http://" + HOST + ":" + str(PORT)
 
@@ -31,72 +30,73 @@ def getConfig():
 
 
 def sendDataRegularly(sensor_list, ptime, version):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(HEADER.format(version).encode('utf8'))
-        data = s.recv(1024)
-        print(data.decode())
-        time.sleep(1)
-        try:
-            while True:
-                for sensor in sensor_list:
-                    data = GPIO.input(sensor["SENSOR_PORT"])
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    s.sendall(HEADER.format(version).encode('utf8'))
+    data = s.recv(1024)
+    print(data.decode())
+    time.sleep(1)
+    try:
+        while True:
+            for sensor in sensor_list:
+                data = GPIO.input(sensor["SENSOR_PORT"])
+                body = {}
+                body['SENSOR_NAME'] = sensor["SENSOR_NAME"]
+                body['SENSOR_PORT'] = sensor["SENSOR_PORT"]
+                body['SENSOR_DATA'] = data
+                print("sending")
+                print(str(json.dumps(body)).encode('utf8'))
+                s.sendall(str(json.dumps(body)).encode('utf8'))
+                ret = s.recv(1024)
+                print(ret.decode())
+                if(ret.decode('utf8') != str(version)):
+                    return
+
+            time.sleep(ptime)
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+
+
+def sendDataUpdates(sensor_list, ptime, version):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    s.sendall(HEADER.format(version).encode('utf8'))
+    data = s.recv(1024)
+    print(data.decode())
+    time.sleep(1)
+
+    data = {}
+    for sensor in sensor_list:
+        data[sensor["SENSOR_PORT"]] = None
+    try:
+        while True:
+            for sensor in sensor_list:
+                port_data = GPIO.input(sensor["SENSOR_PORT"])
+                if port_data != data[sensor["SENSOR_PORT"]]:
+                    data[sensor["SENSOR_PORT"]] = port_data
                     body = {}
                     body['SENSOR_NAME'] = sensor["SENSOR_NAME"]
                     body['SENSOR_PORT'] = sensor["SENSOR_PORT"]
-                    body['SENSOR_DATA'] = data
-                    print("sending")
-                    print(str(json.dumps(body)).encode('utf8'))
+                    body['SENSOR_DATA'] = port_data
                     s.sendall(str(json.dumps(body)).encode('utf8'))
                     ret = s.recv(1024)
                     print(ret.decode())
                     if(ret.decode('utf8') != str(version)):
-                        os.system("sudo reboot")
+                        return
 
-                time.sleep(ptime)
+            time.sleep(ptime)
 
-        except KeyboardInterrupt:
-            GPIO.cleanup()
-
-
-def sendDataUpdates(sensor_list, ptime, version):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(HEADER.format(version).encode('utf8'))
-        data = s.recv(1024)
-        print(data.decode())
-        time.sleep(1)
-
-        data = {}
-        for sensor in sensor_list:
-            data[sensor["SENSOR_PORT"]] = None
-        try:
-            while True:
-                for sensor in sensor_list:
-                    port_data = GPIO.input(sensor["SENSOR_PORT"])
-                    if port_data != data[sensor["SENSOR_PORT"]]:
-                        data[sensor["SENSOR_PORT"]] = port_data
-                        body = {}
-                        body['SENSOR_NAME'] = sensor["SENSOR_NAME"]
-                        body['SENSOR_PORT'] = sensor["SENSOR_PORT"]
-                        body['SENSOR_DATA'] = port_data
-                        s.sendall(str(json.dumps(body)).encode('utf8'))
-                        ret = s.recv(1024)
-                        print(ret.decode())
-                        if(ret.decode('utf8') != str(version)):
-                            os.system("sudo reboot")
-
-                time.sleep(ptime)
-
-        except KeyboardInterrupt:
-            GPIO.cleanup()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
 
 if __name__ == '__main__':
-    send_data_regularly, ptime, sensor_list, version = getConfig()
-    setupPi(sensor_list)
+    while True:
+        send_data_regularly, ptime, sensor_list, version = getConfig()
+        setupPi(sensor_list)
 
-    if send_data_regularly:
-        sendDataRegularly(sensor_list, ptime, version)
-    else:
-        sendDataUpdates(sensor_list, ptime, version)
+        if send_data_regularly:
+            sendDataRegularly(sensor_list, ptime, version)
+        else:
+            sendDataUpdates(sensor_list, ptime, version)
